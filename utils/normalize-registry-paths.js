a4a6220f29690b01b97fc0951d1d82e5a0f5b5bf
Mike Pirog <mike@lando.dev>
2023-01-05 10:14:00 -0500

    Fix bug causing normalize registry paths to fail if registry returns a non-string

--- utils/normalize-registry-paths.js
+++ utils/normalize-registry-paths.js
@@ -7,7 +7,7 @@ const Config = require('../core/config');
 module.exports = (registry = {}, base) => {
   // go through the keys and try to normalize on base
   for (const component of Config.keys(registry)) {
-    if (component && !path.isAbsolute(get(registry, component))) {
+    if (component && typeof get(registry, component) === 'string' && !path.isAbsolute(get(registry, component))) {
       set(registry, component, path.resolve(base, get(registry, component)));
     }
   }
